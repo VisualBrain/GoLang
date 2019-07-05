11f21bfc62e4e8987f49dc3fb3f4cb6a1fbed301
Jatinder Kumar Chaurasia <32101330+JatinderKumarChaurasia@users.noreply.github.com>
2019-07-05 11:44:02 +0530

    Update start.go

--- start.go
+++ start.go
@@ -4,11 +4,15 @@ package main
    if variable is new than use :=
    otherwise to update the previous declared variable use =
    function can return mutliple values
+   a numeric constant has no type until it is given through explicit conversion
 */
 import (
 	"fmt"
+	"math"
 	"os"
 )
+
+const checkconst = "Learning Constant"
 
 func main() {
 	var name string
@@ -16,6 +20,16 @@ func main() {
 	//age =45
 	//other way age:=getAge()
 	//shorter way
+	var a string = "Mutkl"
+	fmt.Println(a)
+	fmt.Println("Printing constant string:", checkconst)
+	const intconst = 5 //A numeric constant has no type
+	converconstint := int64(intconst)
+	fmt.Println("Converted numeric constant to int64:", converconstint)
+	fmt.Println("Using Math.sin():", math.Sin(intconst)) //using math
+	var b, c int = 4, 5
+	fmt.Println(b, c)
+
 	education := "BSLC"
 	if len(os.Args) != 2 {
 		os.Exit(1)
